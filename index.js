7f0836b7ac2db6aa9cea036ff7c473185e06e81b
FabMira <fmirabella08@gmail.com>
2024-09-20 17:23:28 -0300

    exercise 4.1 step 1

--- index.js
+++ index.js
@@ -1,116 +1,42 @@
-require('dotenv').config({path: '.env'}) //dotenv: libreria para gestion de variables de entorno
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
-const Person = require('./person')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-morgan.token('reqContent', function getContent (req) {
-  if(req.method === 'POST') {
-    return JSON.stringify(req.body)
-  }
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :reqContent'))
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
 app.use(cors())
-app.use(express.static('dist'))
 app.use(express.json())
-let persons = []
 
-app.get('/', (req, res) => {
-  res.send('<h1>Phonebook backend</h1>')
-})
-
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(person => {
-    res.json(person)
-    persons = (JSON.parse(JSON.stringify(person)))
-  })
-})
-
-app.get('/info', (req, res) => {
-  const reqTime = new Date()
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${reqTime.toString()}</p>`)
-})
-
-app.get('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
-  Person.findById(id)
-    .then(person => {
-      if(person) {
-        res.json(person)
-      } else {
-        res.status(404).end()
-      }
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
     })
-    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
-  Person.findByIdAndDelete(id)
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
     .then(result => {
-      res.json(result)
-      persons = persons.filter(person => person.name !== result.name)
+      response.status(201).json(result)
     })
-    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res, next) => {
-  const body = req.body
-
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-  /*let err
-  err = person.validateSync()*/
-
-
-  person.save()
-    .then(savedPerson => {
-      res.json(savedPerson)
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
-
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(req.params.id, person, {new: true})
-    .then(updatedPerson => {
-      res.json(updatedPerson)
-    })
-    .catch(error => next(error))
-})
-
-const PORT = process.env.PORT
+const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({error: 'uknown endpoint'})
-}
-//controlador de solicitudes con endpoint desconocido
-app.use(unknownEndpoint)
-
-const errorHandler = (error, req, res, next) => {
-  console.log(error.message)
-
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message })
-  }
-  next(error)
-}
-
-//este debe ser el ultimo middleware cargado, ¡tambien todas las rutas deben ser registradas antes que esto!
-app.use(errorHandler)
