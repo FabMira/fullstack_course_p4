baf7fd93ab7681fcea12f46d86a7b450d0ecae61
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-04-03 14:37:15 -0300

    exercise 4.13 finished - deleting a blog and it's test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -18,56 +18,60 @@ beforeEach(async () => {
     blog = new Blog(helper.initialBlogs[1])
     await blog.save()
 })
-
-test('blogs are returned as json', async () => {
-    await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('content-Type', /application\/json/)
-})
-
-test('there are two blogs', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
-
-test('the first blog is about React patterns', async () => {
-    const response = await api.get('/api/blogs')
-    const titles = response.body.map(e => e.title)
-    assert(titles.includes('React patterns'))
-})
-
-test('the unique identifier is caller "id"', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.forEach(blog => {
-        assert('id' in blog);
+describe('when there is initially some blogs', () => {
+    test('blogs are returned as json', async () => {
+        await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('content-Type', /application\/json/)
+    })
+    
+    test('there are two blogs', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+    
+    test('the first blog is about React patterns', async () => {
+        const response = await api.get('/api/blogs')
+        const titles = response.body.map(e => e.title)
+        assert(titles.includes('React patterns'))
     })
 })
 
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-    
-    const titles = blogsAtEnd.map(n => n.title)
-    assert(titles.includes('Canonical string reduction'))
+describe('proper unique identifier', () => {
+    test('the unique identifier is caller "id"', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach(blog => {
+            assert('id' in blog);
+        })
+    })
 })
 
+describe('test for adding blogs', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('content-Type', /application\/json/)
+    
+        const response = await api.get('/api/blogs')
+    
+        const blogsAtEnd = await helper.blogsInDb()
+    
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+        
+        const titles = blogsAtEnd.map(n => n.title)
+        assert(titles.includes('Canonical string reduction'))
+    })
+})
 // test('blog without author is not added', async () => {
 //     const newBlog = {
 //         title: "Canonical string reduction",
@@ -83,28 +87,29 @@ test('a valid blog can be added', async () => {
 //     const blogsAtEnd = await helper.blogsInDb()
 //     assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 // })
-
-test('if no likes in request, it takes 0 by default', async () => {
-    const newBlog = {
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll"
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-
-    const blogsAtEnd = await helper.blogsInDb();
-    const lastBlogAdded = blogsAtEnd[blogsAtEnd.length-1]
-
-    assert('likes' in lastBlogAdded)
-    assert.strictEqual(0, lastBlogAdded.likes)
+describe('missing likes of the new blog', () => {
+    test('if no likes in request, it takes 0 by default', async () => {
+        const newBlog = {
+            title: "First class tests",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll"
+        }
     
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+    
+        const blogsAtEnd = await helper.blogsInDb();
+        const lastBlogAdded = blogsAtEnd[blogsAtEnd.length-1]
+    
+        assert('likes' in lastBlogAdded)
+        assert.strictEqual(0, lastBlogAdded.likes)
+        
+    })
 })
 
-describe('exercise 4.12 - missing required properties', () => {
+describe('missing required properties', () => {
     const newBlog = {
         title: "Canonical string reduction",
         author: "Edsger W. Dijkstra",
@@ -125,6 +130,23 @@ describe('exercise 4.12 - missing required properties', () => {
             .post('/api/blogs')
             .send(_.omit(newBlog, ['url']))
             .expect(400)
+    })
+})
+
+describe('deleting a blog', () => {
+    test("deleting a blog by it's id", async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api  
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        
+        const titles = blogsAtEnd.map(blog => blog.title)
+        assert(!titles.includes(blogToDelete.title))
+        assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length -1)
     })
 })
 
