eaafa1507b25b380623ef0d016ed3e7bfc9a78d6
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-04-04 23:37:00 -0300

    exercise 4.19 finished - blogs published with jwt

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/users')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response, next) => {
     try {
@@ -11,12 +12,22 @@ blogsRouter.get('/', async (request, response, next) => {
     }
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
-    const user = await User.findById(request.body.userId)
-    
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if( !decodedToken.id ){
+        return response.status(401).json({error: 'invalid token'})
+    }
+    const user = await User.findById(decodedToken.id)
     const blog = new Blog(request.body)
     blog.user = user.id
-
     try {
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
